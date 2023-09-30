a66134a4f17000c89a14769f783082f6f31b3af2
Ayush Dadhich <ayushdadhich900@gmail.com>
2023-09-30 18:24:16 +0530

    Display the list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-demo';
-   HEROES = [
-    { id: 12, name: 'Dr. Nice' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr. IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-  ];
+  title = 'Tour of Heroes';
 
-  saveinput() {
-    alert('Input Added')
-  }
 }
